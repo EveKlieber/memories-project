840087aab4449df6f69c226a98623069cbd1682f
a-champi <a.champi.h@gmail.com>
2022-05-12 08:21:47 +0200

    included deletePost to post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -33,10 +33,20 @@ const updatePost = async(req,res) =>{
 
   if(!mongoose.Types.ObjectId.isValid()) return res.status(404).send('No post with that id');
   
-  const  updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id} {new: true});
+  const  updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
 
   res.json(updatedPost)
 
-
 }
-export { getPosts, createPost, updatePost }
+
+const deletePost = async(req, res) => {
+  const {id} = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid()) return res.status(404).send('No post with that id');
+
+  await PostMessage.findByIdAndDelete(id);
+
+  res.json({message:' Post deleted'})
+}
+
+export { getPosts, createPost, updatePost, deletePost }
